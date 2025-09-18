0168c04044707e4c6cb6cccba64709e47b34d0d9
Miloš Kričković <miloshino3@gmail.com>
2025-09-18 13:43:51 +0200

    Dodat html fajl, dodata klasa sa pocetnim vrednostima u JS

---
+++ example.js
@@ -0,0 +1,14 @@
+class Artikal{
+    constructor(jedinstvenBroj,naziv,cena,dostupnost) {
+        this.jedinstvenBroj
+        this.naziv
+        this.cena
+        this.dostupnost
+    }
+}
+
+let artikli = [
+    new Artikal(1, "Monitor", 165, true),
+    new Artikal(2, "Tv", 650, false),
+    new Artikal(3, "Mis", 20, true),
+]
