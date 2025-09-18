8767c7f80d91a3c561b4300352b6038449991753
Miloš Kričković <miloshino3@gmail.com>
2025-09-18 16:14:36 +0200

    Dodat stil za table, logika u JS fajl

--- example.js
+++ example.js
@@ -1,14 +1,46 @@
-class Artikal{
-    constructor(jedinstvenBroj,naziv,cena,dostupnost) {
-        this.jedinstvenBroj
-        this.naziv
-        this.cena
-        this.dostupnost
+'use strict'
+
+class Artikal {
+    constructor(id, naziv, cena, dostupan) {
+        this.id = id
+        this.naziv = naziv
+        this.cena = cena
+        this.dostupan = dostupan
     }
 }
 
 let artikli = [
-    new Artikal(1, "Monitor", 165, true),
-    new Artikal(2, "Tv", 650, false),
-    new Artikal(3, "Mis", 20, true),
+    new Artikal(1, "Laptop", 165, true),
+    new Artikal(2, "TV", 650, false),
+    new Artikal(3, "Mis", 20, true)
 ]
+
+let tabela = document.querySelector("#artikli")
+
+for (let i = 0; i < artikli.length; i++) {
+
+    let artikal = artikli[i]
+    let red = document.createElement("tr")
+    let id = document.createElement("td")
+    let naziv = document.createElement("td")
+    let cena = document.createElement("td")
+    let dostupnost = document.createElement("td")
+
+    id.textContent = artikal.id
+    naziv.textContent = artikal.naziv
+    cena.textContent = artikal.cena
+
+    if (artikal.dostupan === true) {
+        dostupnost.textContent = "DA"
+    } else {
+        dostupnost.textContent = "NE"
+        red.style.backgroundColor ="#FFB6B6"
+    }
+   
+    red.appendChild(id)
+    red.appendChild(naziv)
+    red.appendChild(cena)
+    red.appendChild(dostupnost)
+
+    tabela.appendChild(red)
+}
